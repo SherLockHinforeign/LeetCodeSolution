6c7f5dc834b2e910fbb0ddc15ce63b74566f86e1
SherLock <735033781@qq.com>
2020-09-11 16:36:27 +0800

    20200911.LeetCode.SecondSolution

---
+++ src/two/Solution.java
@@ -0,0 +1,26 @@
+package two;
+
+/**
+ * 
+ * @author 73503
+ *   	题目：給出兩個非空的鏈表用來表示兩個非負的整數，其中他們各自的位數是按照逆序的方式存儲，并且每個節點只能存儲一位數字，
+ *   		返回一個新的鏈表表示他們的和
+ *  	 实例：
+ *   		輸入：
+ *   		輸出：
+ *
+ */
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// 測試案例
+		
+	}
+	
+	// Solution1
+	public static void solution1() {
+		
+	}
+	
+}
